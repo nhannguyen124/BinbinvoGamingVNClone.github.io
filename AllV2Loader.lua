6d0f6130ee6f39684d845495ac51d32e57fae356
BinbinvoGamingVNN <122621965+BinbinvoGamingVNClone@users.noreply.github.com>
2024-11-28 10:08:45 +0700

    Create AllV2Loader.lua

---
+++ AllV2Loader.lua
@@ -0,0 +1,122 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+repeat wait() until game:IsLoaded()
+
+local PlaceId = game.PlaceId
+
+
+if PlaceId == 1537690962 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/BeeSwarmSimulator/Loader'))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+elseif PlaceId == 3101667897 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/Game3',true))()
+   OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+elseif PlaceId == 4490140733 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/MyRestaurant/Loader'))()
+
+    warn('Execute success')
+
+elseif PlaceId == 606849621 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/Game2',true))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+elseif PlaceId == 537413528 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/Game1',true))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+elseif PlaceId == 2753915549 or 4442272183 or 7449423635 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/BinbinvoGamingCommmunty/main/Blox%20Fruits'))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+elseif PlaceId == 3101667897 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/BinbinvoGamingCommmunty/main/Legends%20Of%20Speed'))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+elseif PlaceId == 6766156863 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/Strongmansimulatorgui.lua'))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+elseif PlaceId == 6516141723 then
+
+    loadstring(game:HttpGet(('https://BinbinvoGamingVNClone.github.io/Doors/V2/Loader'),true))()
+
+elseif PlaceId == 6284583030 then
+
+    loadstring(game:HttpGet(('https://BinbinvoGamingVNClone.github.io/Evade'),true))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+elseif PlaceId == 142823291 then
+
+    loadstring(game:HttpGet(('https://BinbinvoGamingVNClone.github.io/MurderMystery2/Loader'),true))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+elseif PlaceId == 16732694052 then
+
+    loadstring(game:HttpGet("https://BinbinvoGamingVNClone.github.io/Fisch/V2/Loader",true))()
+
+elseif PlaceId == 10260193230 then
+
+    loadstring(game:HttpGet("https://BinbinvoGamingVNClone.github.io/MemeSea/Fixed/Loader",true))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+else
+    game.Players.LocalPlayer:Kick('Not Support this game.')
+
+end

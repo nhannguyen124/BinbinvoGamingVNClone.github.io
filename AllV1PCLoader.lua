15e99da6498408710b338446c799af1a85a5f11f
BinbinvoGamingVNN <122621965+BinbinvoGamingVNClone@users.noreply.github.com>
2024-11-28 10:19:55 +0700

    Create AllV1PCLoader.lua

---
+++ AllV1PCLoader.lua
@@ -0,0 +1,63 @@
+repeat wait() until game:IsLoaded()
+
+local PlaceId = game.PlaceId
+
+
+if PlaceId == 1537690962 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/BeeSwarmSimulator/Loader'))()
+
+elseif PlaceId == 3101667897 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/Game3',true))()
+elseif PlaceId == 4490140733 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/MyRestaurant/Loader'))()
+
+    warn('Execute success')
+
+elseif PlaceId == 606849621 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/Game2',true))()
+
+elseif PlaceId == 537413528 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/Game1',true))()
+
+elseif PlaceId == 2753915549 or 4442272183 or 7449423635 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/BinbinvoGamingCommmunty/main/Blox%20Fruits'))()
+
+elseif PlaceId == 3101667897 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/BinbinvoGamingCommmunty/main/Legends%20Of%20Speed'))()
+
+elseif PlaceId == 6766156863 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/Strongmansimulatorgui.lua'))()
+
+elseif PlaceId == 6516141723 then
+
+    loadstring(game:HttpGet(('https://BinbinvoGamingVNClone.github.io/Doors/V1/Loader'),true))()
+
+elseif PlaceId == 6284583030 then
+
+    loadstring(game:HttpGet(('https://BinbinvoGamingVNClone.github.io/Evade'),true))()
+
+elseif PlaceId == 142823291 then
+
+    loadstring(game:HttpGet(('https://BinbinvoGamingVNClone.github.io/MurderMystery2/Loader'),true))()
+
+elseif PlaceId == 16732694052 then
+
+    loadstring(game:HttpGet("https://BinbinvoGamingVNClone.github.io/Fisch/V2/Loader",true))()
+
+elseif PlaceId == 10260193230 then
+
+    loadstring(game:HttpGet("https://BinbinvoGamingVNClone.github.io/MemeSea/Fixed/Loader",true))()
+
+else
+
+    game.Players.LocalPlayer:Kick('Not Support this game.')
+
+end

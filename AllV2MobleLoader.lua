d384f4e714da5ad341ade8b3277313fd30d10a04
BinbinvoGamingVNN <122621965+BinbinvoGamingVNClone@users.noreply.github.com>
2024-11-28 10:17:42 +0700

    Create AllV2MobleLoader.lua

---
+++ AllV2MobleLoader.lua
@@ -0,0 +1,74 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+repeat wait() until game:IsLoaded()
+
+local PlaceId = game.PlaceId
+
+if PlaceId == 1537690962 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/BeeSwarmSimulator/Loader'))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+    warn('Execute success')
+
+elseif PlaceId == 4490140733 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/MyRestaurant/Loader'))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+elseif PlaceId == 3101667897 then
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/BinbinvoGaming/DeletedUserCode/main/BuildABoatForTreasure/GoldFarm'))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+elseif PlaceId == 6516141723 then
+
+    loadstring(game:HttpGet(('https://BinbinvoGaming.github.io/Doors/V2/Loader'),true))()
+
+elseif PlaceId == 6284583030 then
+
+    loadstring(game:HttpGet(('https://BinbinvoGaming.github.io/Evade'),true))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+elseif PlaceId == 16732694052 then
+
+    loadstring(game:HttpGet("https://BinbinvoGamingVNClone.github.io/Fisch/V2/Loader",true))()
+
+elseif PlaceId == 142823291 then
+
+    loadstring(game:HttpGet(('https://BinbinvoGaming.github.io/MurderMystery2/Loader'),true))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+elseif PlaceId == 10260193230 then
+
+    loadstring(game:HttpGet("https://binbinvogaming.github.io/MemeSea/Fixed/Loader",true))()
+OrionLib:MakeNotification({
+	Name = "BinbinvoGaming Hub",
+	Content = "The Game Not Release V2",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+else
+
+    game.Players.LocalPlayer:Kick('Not Support this map.')
+
+end
